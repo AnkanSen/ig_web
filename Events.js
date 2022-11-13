c646f4ec129e55736dd0de501ddd0631e14fa06d
Ankan Sen <maharajankan@gmail.com>
2022-11-13 12:10:26 +0530

    Add files via upload

---
+++ Events.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import EventTimeline from '../components/EventTimeline'
+const Events = () => {
+  return (
+    <>
+    <EventTimeline/>
+    </>
+  )
+}
+
+export default Events
