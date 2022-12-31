220576703f025e09530739c6965814bf79023006
Ankan Sen <maharajankan@gmail.com>
2022-12-31 12:42:59 +0530

    Add files via upload

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,9 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
 import logo from '../assets/img/logo.png';
 
 
@@ -31,7 +31,7 @@ function NavBar() {
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="/">
           <Link to="/" onClick={handleOnClick}>
             <img src={logo} alt="Logo" />
           </Link>
